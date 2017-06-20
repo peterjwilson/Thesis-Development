ef9c025545eca461cf5553e47673497903d4180a
peterjwilson <peter.wilson@tum.de>
2017-06-20 10:24:43 +0200

    update

---
+++ Thick triangle shell/Literature/DSG_20130823/DSGc3v2.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 10:26:16 2017
+
+@author: Peter Wilson
+"""
+
+import numpy as np
+import sympy as sp
+print("Sympy version: ",sp.__version__)
+
+x = sp.Symbol('x')
+y = sp.Symbol('y')
+
+alpha = sp.Symbol('alpha')
+beta = sp.Symbol('beta')
+
+a1 = sp.Symbol('a1')
+a2 = sp.Symbol('a2')
+a3 = sp.Symbol('a3')
+a4 = sp.Symbol('a4')
+a5 = sp.Symbol('a5')
+a6 = sp.Symbol('a6')
+a7 = sp.Symbol('a7')
+a8 = sp.Symbol('a8')
+a9 = sp.Symbol('a9')
+
+Bub = sp.Symbol('Bub')
+
+W1 = sp.Symbol('W1')
+W2 = sp.Symbol('W2')
+W3 = sp.Symbol('W3')
+PX1 = sp.Symbol('PX1')
+PX2 = sp.Symbol('PX2')
+PX3 = sp.Symbol('PX3')
+PY1 = sp.Symbol('PY1')
+PY2 = sp.Symbol('PY2')
+PY3 = sp.Symbol('PY3')
+
+a = sp.Symbol('a') #x2 - x1
+b = sp.Symbol('b') #y2 - y1
+c = sp.Symbol('c') #y3 - y1
+d = sp.Symbol('d') #x3 - x1
+detJ = sp.Symbol('detJ')
+
+
+#Nodal coords
+x1 = sp.Symbol('x1')
+y1 = sp.Symbol('y1')
+x2 = sp.Symbol('x2')
+y2 = sp.Symbol('y2')
+x3 = sp.Symbol('x3')
+y3 = sp.Symbol('y3')
+
+
+#Node coordinates set to example values
+x1 = 0.0
+y1 = 0.0
+x2 = 1.0
+y2 = 0.0
+x3 = 0.0
+y3 = 1.0
+
+# Section 1 ---------------------------------------------------
+PHI = a1 + a2*x +a3*y + a4*x**2 + 0.5*(a5+a6)*x*y + a7*y**2
+
+#GAM = a8*x + a9*y - Bub*x*y #generic bubble mode
+#GAM = a8*x + a9*y - (a8+a9)*x*y #generic bubble mode
+GAM = a8*x + a9*y #no bubble
+
+PD = 0.5*(a5-a6)*x*y
+C = a5 - a6
+
+# Section 2 ---------------------------------------------------
+PX = PHI + PD
+PY = PHI - PD
+#GX = GAM + PD
+#GY = GAM - PD
+GX = a8*x + a9*y #normal DSG
+GY = a8*x + a9*y #normal DSG
+
+# Section 3 ---------------------------------------------------
+px = sp.diff(PX,x)
+py = sp.diff(PY,y)
+gx = sp.diff(GX,x)
+gy = sp.diff(GY,y)
+
+print("\nSymbolic gx:",gx)
+print("\nSymbolic gy:",gy)
+
+# Section 4 ---------------------------------------------------
+# Skip internal energy derivation, just write bubble mode result
+Bub = a8 + a9
+
+# Alternative representation of the displacement field as the difference between thrust and Kirchhoff
+W = GAM - PHI
+WX = GX - PX
+WY = GY - PY
+
+#Identification of the Ansatz coefficients with the node values for shifts Wi and rotations
+# (converted to zero value eqns)
+eq1 = W1 - (W.subs(x,x1)).subs(y,y1)
+eq2 = W2 - (W.subs(x,x2)).subs(y,y2)
+eq3 = W3 - (W.subs(x,x3)).subs(y,y3)
+
+eq4 = PX1 - (px.subs(x,x1)).subs(y,y1)
+eq5 = PX2 - (px.subs(x,x2)).subs(y,y2)
+eq6 = PX3 - (px.subs(x,x3)).subs(y,y3)
+
+eq7 = PY1 - (py.subs(x,x1)).subs(y,y1)
+eq8 = PY2 - (py.subs(x,x2)).subs(y,y2)
+eq9 = PY3 - (py.subs(x,x3)).subs(y,y3)
+
+# Setup system to solve [A][a] = [W] -------------------------
+A = sp.zeros(9) #system matrix
+UVector = sp.zeros(9,1) #vector of displacements
+UVector[0] = W1
+UVector[1] = W2
+UVector[2] = W3
+UVector[3] = PX1
+UVector[4] = PX2
+UVector[5] = PX3
+UVector[6] = PY1
+UVector[7] = PY2
+UVector[8] = PY3
+
+results = list(sp.linsolve([eq1,eq2,eq3,eq4,eq5,eq6,eq7,eq8,eq9],[a1,a2,a3,a4,a5,a6,a7,a8,a9]))
+ansatzCoefficients = results[0]
+print("\nAnsatz coefficients solved",ansatzCoefficients)
+
+#Go through and update everything "_u"
+PHI_u = PHI.subs([(a1,ansatzCoefficients[0]),(a2,ansatzCoefficients[1]),(a3,ansatzCoefficients[2]),(a4,ansatzCoefficients[3]),(a5,ansatzCoefficients[4]),(a6,ansatzCoefficients[5]),(a7,ansatzCoefficients[6]),(a8,ansatzCoefficients[7]),(a9,ansatzCoefficients[8])])
+
+GAM_u = GAM.subs([(a1,ansatzCoefficients[0]),(a2,ansatzCoefficients[1]),(a3,ansatzCoefficients[2]),(a4,ansatzCoefficients[3]),(a5,ansatzCoefficients[4]),(a6,ansatzCoefficients[5]),(a7,ansatzCoefficients[6]),(a8,ansatzCoefficients[7]),(a9,ansatzCoefficients[8])])
+
+PD_u = PD.subs([(a1,ansatzCoefficients[0]),(a2,ansatzCoefficients[1]),(a3,ansatzCoefficients[2]),(a4,ansatzCoefficients[3]),(a5,ansatzCoefficients[4]),(a6,ansatzCoefficients[5]),(a7,ansatzCoefficients[6]),(a8,ansatzCoefficients[7]),(a9,ansatzCoefficients[8])])
+
+#C_u = C.subs([(a1,ansatzCoefficients[0]),(a2,ansatzCoefficients[1]),(a3,ansatzCoefficients[2]),(a4,ansatzCoefficients[3]),(a5,ansatzCoefficients[4]),(a6,ansatzCoefficients[5]),(a7,ansatzCoefficients[6]),(a8,ansatzCoefficients[7]),(a9,ansatzCoefficients[8])])
+
+
+#PX_u = PHI_u + PD_u
+#PY_u = PHI_u - PD_u
+#GX_u = GAM_u + PD_u
+#GY_u = GAM_u - PD_u
+
+#normal DSG
+GX_u = GX.subs([(a1,ansatzCoefficients[0]),(a2,ansatzCoefficients[1]),(a3,ansatzCoefficients[2]),(a4,ansatzCoefficients[3]),(a5,ansatzCoefficients[4]),(a6,ansatzCoefficients[5]),(a7,ansatzCoefficients[6]),(a8,ansatzCoefficients[7]),(a9,ansatzCoefficients[8])])
+GY_u = GY.subs([(a1,ansatzCoefficients[0]),(a2,ansatzCoefficients[1]),(a3,ansatzCoefficients[2]),(a4,ansatzCoefficients[3]),(a5,ansatzCoefficients[4]),(a6,ansatzCoefficients[5]),(a7,ansatzCoefficients[6]),(a8,ansatzCoefficients[7]),(a9,ansatzCoefficients[8])])
+
+
+# Section 3 ---------------------------------------------------
+#px_u = sp.diff(PX_u,x)
+#py_u = sp.diff(PY_u,y)
+gx_u = sp.diff(GX_u,x)
+gy_u = sp.diff(GY_u,y)
+
+print("\nSymbolic GX_u:",GX_u)
+print("\nSymbolic GY_u:",GY_u)
+
+print("\nSymbolic B Matrix (probably parametric, x and y here mean alpha and beta maybe?) = ")
+
+# Assemble B Matrix ------------------------------------------
+B = sp.zeros(2,9)
+for col in range(9):
+    B[0,col] = sp.diff(gx_u,UVector[col])
+    B[1,col] = sp.diff(gy_u,UVector[col])
+
+print("\nB Matrix for [0,0] [1,0] [0,1] = ")
+sp.pprint(B,wrap_line=False)
+
+#attempted conversion from parametric to cartesian
+
+# d()/dx = d()/dalpha dalpha/dx + d()/dbeta dbeta/dx
+# d()/dy = d()/dalpha dalpha/dy + d()/dbeta dbeta/dy
+
+print("\n\nSymbolic B Matrix (cartesian, x and y here are cartesian) = ")
+for col in range(9):
+    B[0,col] = sp.diff(sp.diff(GX_u,x),UVector[col])*c/detJ + sp.diff(sp.diff(GX_u,y),UVector[col])*-b/detJ
+    B[1,col] = sp.diff(sp.diff(GY_u,x),UVector[col])*-d/detJ + sp.diff(sp.diff(GY_u,y),UVector[col])*a/detJ
+sp.pprint(sp.factor(B)*detJ,wrap_line=False) #detJ taken out for clarity
+print("\nPrinting individual entries of matrix above:")
+Bsimp = sp.factor(B)*detJ #detJ taken out for clarity
+for col in range(9):
+    print("BSuper(0,",col,")=",Bsimp[0,col],";")
+for col in range(9):
+    print("BSuper(1,",col,")=",Bsimp[1,col],";")
